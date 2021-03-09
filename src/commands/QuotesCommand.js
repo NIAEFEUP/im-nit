4c4f3968d88c4e34cc35d13a2a5165c9110de6fb
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-03-09 00:51:41 +0000

    Change quotes to an object and added random quote

--- src/commands/QuotesCommand.js
+++ src/commands/QuotesCommand.js
@@ -6,17 +6,20 @@ module.exports = class QuotesCommand extends BaseCommand {
     }
 
     async run(client, message, args){
-        let quotes = [
-            {"id": "message"}, 
-            {"id2": "message2"}
-        ];
-        //message.channel.send("sent");
-        switch(args[0]){
-            case '1':
-                message.channel.send('Random quote 1');
-                break;
-            default:
-                break;
+        let quotes = {
+            "rui" : "O Rui foi membro do IEEE",
+            "teste" : "Esta quote é só um teste"
+        };
+        if (args[0]){
+            if (quotes[args[0].toLowerCase()])
+                message.channel.send(quotes[args[0].toLowerCase()]);
+            else
+                message.channel.send("Provide an existing quote or just type !quote for a random one");
+        }
+        else {
+            const quoteValues = Object.values(quotes);
+            const randomQuote = quoteValues[quoteValues.length * Math.random() << 0];
+            message.channel.send(randomQuote);
         }
     }
 }
