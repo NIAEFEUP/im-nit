888a8bd00da7a627dce80afb5ccefeffede12eb3
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-03-15 19:33:00 +0000

    Added add and remove quotes, and changed .txt to .json

--- src/commands/QuotesCommand/QuotesCommand.js
+++ src/commands/QuotesCommand/QuotesCommand.js
@@ -1,24 +1,20 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
-var fs = require('fs');
-var fileUtils = require('./utils');
+const fs = require('fs');
+const { writeToFile } = require('./utils');
+
+const filePath = 'src/commands/QuotesCommand/quotes.json';
 
 module.exports = class QuotesCommand extends BaseCommand {
     constructor() {
         super('quote', 'textAnswers', []);
 
         this.quotes = {};
-        fs.readFile('src/commands/QuotesCommand/quotes.txt', 'utf8', (err, contents) => {
+        fs.readFile(filePath, 'utf8', (err, contents) => {
             if (err) {
                 console.error(err);
                 return;
             }
-
-            let res = contents.split('\r\n');
-
-            res.map(quote => {
-                let parsing = quote.split(':');     // contains an array with 2 elements (quoteKey, quoteText)
-                this.quotes[parsing[0].trim()] = parsing[1].trim();
-            })
+            this.quotes = JSON.parse(contents);
             console.log(this.quotes);
         });
     }
@@ -27,12 +23,63 @@ module.exports = class QuotesCommand extends BaseCommand {
         if (args[0]) {
             if (this.quotes[args[0].toLowerCase()])
                 message.channel.send(this.quotes[args[0].toLowerCase()]);
-            else
-                message.channel.send("Provide an existing quote or just type !quote for a random one");
+            else {
+                switch (args[0]) {
+                    case 'add':
+                        if (!args[1] || !args[2]) {
+                            message.channel.send("Wrong format. To add a quote, type:\n" +
+                            client.prefix + "quote add [NAME] [NEW QUOTE]");
+                            return;
+                        }
+
+                        if (this.quotes[args[1]]) {
+                            message.channel.send("A quote with that name already exists! Choose another name or delete it");
+                            return;
+                        }
+
+                        if (args[1] === "add") {
+                            message.channel.send("How do you call your quote if you name it 'add' ? Choose another name");
+                            return;
+                        } else if (args[1] === "delete") {
+                            message.channel.send("How do you call your quote if you name it 'delete' ? Choose another name");
+                            return;
+                        }
+
+                        let newQuote = "";
+                        for (let i = 2; i < args.length; ++i)
+                            newQuote += " " + args[i];
+
+                        this.quotes[args[1]] = newQuote;
+                        writeToFile(filePath, this.quotes);
+                        message.channel.send("Quote added successfully! :D");
+                        break;
+                    case 'delete':
+                        if (!args[1]) {
+                            message.channel.send("Wrong format. To delete a quote, type\n" +
+                            client.prefix + "quote delete [NAME]");
+                            return;
+                        }
+
+                        if (!this.quotes[args[1]]) {
+                            message.channel.send("That quote doesn't exist dummy");
+                            return;
+                        }
+
+                        delete this.quotes[args[1]];
+                        writeToFile(filePath, this.quotes);
+                        message.channel.send("Quote removed successfully! :D");
+                        break;
+                    default:
+                        message.channel.send("That wasn't very nit of you. Use the command in one of the several ways:\n" +
+                            client.prefix + "quote [NAME]\n" +
+                            client.prefix + "quote add [NAME] [NEW QUOTE]\n" +
+                            client.prefix + "quote delete [NAME]");
+                }
+            }
         }
         else {
             const quoteValues = Object.values(this.quotes);
-            const randomQuote = quoteValues[this.quoteValues.length * Math.random() << 0];
+            const randomQuote = quoteValues[quoteValues.length * Math.random() << 0];
             message.channel.send(randomQuote);
         }
     }
--- src/commands/QuotesCommand/utils.js
+++ src/commands/QuotesCommand/utils.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 const writeToFile = (filePath, data) => {
-    fs.writeFile(filePath, data, (err) => {
+    fs.writeFile(filePath, JSON.stringify(data, null, 1), (err) => {
         if(err){
             console.error(err);
             return;
@@ -10,24 +10,6 @@ const writeToFile = (filePath, data) => {
     return;
 }
 
-const readQuotesFromFile = async (filePath) => {
-    let quotes = {};
-    fs.readFile(filePath, 'utf8', (err, contents )=> {
-        if(err){
-            console.error(err);
-            return;
-        }
-        
-        let res = contents.split('\r\n');
-        
-        res.map(quote => {
-            let parsing = quote.split(':');     // contains an array with 2 elements (quoteKey, quoteText)
-            quotes[parsing[0].trim()] = parsing[1].trim();
-        })
-        console.log(quotes);
-    });
-    return quotes;
+module.exports = {
+    writeToFile
 }
-
-exports.writeToFile = writeToFile;
-exports.readQuotesFromFile = readQuotesFromFile;
