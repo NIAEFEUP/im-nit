00ca86430eee7917630c0c84423f1ae00f85710f
monkin77 <monkinsmurf@gmail.com>
2021-04-17 14:45:07 +0100

    Created .env.template file, update .env, changed the format of the reminderCommand

--- src/utils/mongo.js
+++ src/utils/mongo.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const mongoPath = process.env.mongoPath;
+const mongoPath = process.env.MONGO_PATH;
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-    return mongoose;
 }
